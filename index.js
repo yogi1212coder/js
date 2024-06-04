23c838586f63114da887a1e717f1b11085f0fb79
yogi1212coder <159631637+yogi1212coder@users.noreply.github.com>
2024-06-04 15:19:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+var a = "ajay";
+var b = 46237462374;
+var c = 0.46237462374;
+var d = true;
+var e = false;
+var x = BigInt("1243245455555555555455445");
+var y = null;
+var z = Symbol(a);
+var i;
+// let user = new Object();
+let user = {    
+    name: "John",  
+    age: 30        
+  };
+  console.log(user.name);
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+console.log(typeof(user));
+console.log(typeof(i));
