ff10b073ad4a6c87955300844d132d7c28e2a6e3
yogi1212coder <159631637+yogi1212coder@users.noreply.github.com>
2024-07-09 17:09:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
-var a = "ajay";
-var b = 46237462374;
-var c = 0.46237462374;
-var d = true;
-var e = false;
-var x = BigInt("1243245455555555555455445");
-var y = null;
-var z = Symbol(a);
-var i;
-// let user = new Object();
-let user = {    
-    name: "John",  
-    age: 30        
-  };
-  console.log(user.name);
+// var a = "ajay";
+// var b = 46237462374;
+// var c = 0.46237462374;
+// var d = true;
+// var e = false;
+// var x = BigInt("1243245455555555555455445");
+// var y = null;
+// var z = Symbol(a);
+// var i;
+// // let user = new Object();
+// let user = {
+//     name: "John",
+//     age: 30
+//   };
+//   console.log(user.name);
 
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
-console.log(typeof(d));
-console.log(typeof(e));
-console.log(typeof(x));
-console.log(typeof(y));
-console.log(typeof(z));
-console.log(typeof(user));
-console.log(typeof(i));
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(typeof(z));
+// console.log(typeof(user));
+// console.log(typeof(i));
+
+
+//  age =prompt("");
+
+// age > 18 ?console.log( "adult" ):console.log( "not adult");
+  
+
+
+
+
+
+// LOOPS
+
+
+// for Loop
+
+// for (let i = 1; i <= 100; i=i+2) {
+//   console.log(i)
+// }
+
+
+// while Loop
+// while (a <= 10) {
+  // a = 1;
+
+//   a++;
+//   alert(a);
+
+//   // do some work
+// }
+
+//   a = 0;
+// do {
+//   a=a+2;
+//   console.log(a);
+// } while (a <= 100 );
+
+
+
+
+let str = "ajay";
+console.log(str)
+// console.log(str.toLowerCase( ))
+console.log(str.toUpperCase())
+console.log(str.trim( ))
+console.log(str.length)
+console.log(str[0],str[1],str[2],str[3] ,str[4])
+
+
+// let str = `string text ${expression} string text`;
